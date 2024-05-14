603dea74ffd88a1c826f53a81e664a4667b0f9d8
MiriamCall <bow18005@byui.edu>
2024-05-14 11:15:04 -0600

    Refactor HTML and CSS to add dark mode functionality

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -13,3 +13,19 @@ function getLastModified() {
 
 getYear();
 getLastModified();
+
+// Dark Mode//
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+  if (modeButton.textContent.includes("🕶️")) {
+    body.style.background = "#000";
+    body.style.color = "#fff";
+    modeButton.textContent = "🔆";
+  } else {
+    body.style.background = "#fff";
+    body.style.color = "#000";
+    modeButton.textContent = "🕶️";
+  }
+});
