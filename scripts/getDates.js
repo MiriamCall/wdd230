a66d7f136f414fa6127d5aa4632aa47b890fc9f6
MiriamCall <bow18005@byui.edu>
2024-05-16 10:24:20 -0600

    Refactor HTML and CSS to add hamburger menu for improved navigation

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -29,3 +29,12 @@ modeButton.addEventListener("click", () => {
     modeButton.textContent = "🕶️";
   }
 });
+
+//hamburger menu//
+const hamburger = document.querySelector(".nav");
+const hambutton = document.querySelector("#menu");
+
+hambutton.addEventListener("click", () => {
+  hamburger.classList.toggle("show");
+  hambutton.classList.toggle("show");
+});
