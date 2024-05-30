cd7e1d3aa30d9d2fcd9801fbada4b859802002bb
MiriamCall <bow18005@byui.edu>
2024-05-30 09:58:56 -0600

    Add lazy loading for images in discover page

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -2,10 +2,10 @@ const visits = document.querySelector(".visits");
 
 let numVisits = Number(localStorage.getItem("numVisits")) || 0;
 
-if (numVisits !== 0) {
+if (numVisits === 0) {
   visits.textContent = `You have visited this site ${numVisits} times`;
 } else {
-  visits.textContent = `This is your first visit. Welcome!`;
+  visits.textContent = `This is your first visit, Welcome!`;
 }
 
 numVisits++;
