428afbbb732ab138e441fbab0618a5d2ef156e32
MiriamCall <bow18005@byui.edu>
2024-06-27 10:20:38 -0600

    refactor: Refactor links.js to improve code structure and readability

--- scripts/links.js
+++ scripts/links.js
@@ -6,18 +6,28 @@ async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
   console.log(data);
-  displayLinks(data);
+  displayLinks(data.weeks);
 }
 getLinks();
 
 function displayLinks(data) {
   const links = document.querySelector("#links");
-  data.forEach((link) => {
-    const linkItem = document.createElement("li");
-    const linkElement = document.createElement("a");
-    linkElement.setAttribute("href", `${baseURL}/${link.path}`);
-    linkElement.textContent = link.label;
-    linkItem.appendChild(linkElement);
-    links.appendChild(linkItem);
+  data.forEach((week) => {
+    const weekItem = document.createElement("li");
+    weekItem.textContent = week.week;
+
+    week.links.forEach((link) => {
+      const linkItem = document.createElement("li");
+      const linkElement = document.createElement("a");
+      linkElement.setAttribute("href", `${baseURL}/${link.path}`);
+      linkElement.textContent = link.label;
+      linkItem.appendChild(linkElement);
+      weekItem.appendChild(linkItem);
+    });
+    //   const linkElement = document.createElement("a");
+    //   linkElement.setAttribute("href", `${baseURL}/${link.path}`);
+    //   linkElement.textContent = link.label;
+    //   linkItem.appendChild(linkElement);
+    //   links.appendChild(linkItem);
   });
 }
