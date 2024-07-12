18e73c1e0b37108ea47e12a6979b274fd229f8aa
MiriamCall <bow18005@byui.edu>
2024-07-11 20:55:10 -0600

    Update Chamber directory with improved member card layout, lazy loading for images, and additional member information

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -21,9 +21,9 @@ async function getMemberData() {
   const response = await fetch(url);
   const data = await response.json();
 
-  console.table(data.members);
-  console.log(data.members[0].name);
-  displayMembers(data.members);
+  console.table(data);
+  console.log(data[0].name);
+  displayMembers(data);
 }
 
 function displayMembers(members) {
